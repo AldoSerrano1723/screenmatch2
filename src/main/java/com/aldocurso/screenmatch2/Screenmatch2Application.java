b40df104a72fc4e3bb4e669474b6bc06702da07d
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-12-02 09:05:17 -0600

    feat: Añadir el record DatosEpisodio

--- src/main/java/com/aldocurso/screenmatch2/Screenmatch2Application.java
+++ src/main/java/com/aldocurso/screenmatch2/Screenmatch2Application.java
@@ -1,5 +1,6 @@
 package com.aldocurso.screenmatch2;
 
+import com.aldocurso.screenmatch2.model.DatosEpisodio;
 import com.aldocurso.screenmatch2.model.DatosSerie;
 import com.aldocurso.screenmatch2.service.ConsumoAPI;
 import com.aldocurso.screenmatch2.service.ConvertirDatos;
@@ -23,13 +24,22 @@ public class Screenmatch2Application implements CommandLineRunner {
         var consumoApi = new ConsumoAPI();
         var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&&apikey=eca75ade");
 
+        //MOSTRANDO JASON CRUDO
         System.out.println("Json crudo:");
         System.out.println(json);
         System.out.println("\n");
 
+        //MOSTRANDO UN OBJETO TIPO SERIE
         ConvertirDatos conversor = new ConvertirDatos();
-        DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
-        System.out.println("Json convetido a un objeto de java:");
-        System.out.println(datos);
+        var datosSerie = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println("Json convetido a un objeto tipo 'Serie':");
+        System.out.println(datosSerie);
+        System.out.println("\n");
+
+        //MOSTRANDO UN OBJETO TIPO EPISODIO
+        json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&Episode=1&apikey=eca75ade");
+        var datosEpisodio = conversor.obtenerDatos(json, DatosEpisodio.class);
+        System.out.println("Json convetido a un objeto tipo 'Episodio':");
+        System.out.println(datosEpisodio);
     }
 }
